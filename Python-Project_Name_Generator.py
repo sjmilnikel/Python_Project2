49391dbf0e794f831637640ab0811b85a00748ce
Sara Charron-Milnikel <sjmilnikel@yahoo.com>
2022-12-11 04:24:30 +0000

    complete

--- Python-Project_Name_Generator.py
+++ Python-Project_Name_Generator.py
@@ -1,31 +1,23 @@
 #!/usr/bin/env python3.7
 
 import random
+import string
 
-# EC2 instances - names, number, department
-
-# How many EC2s they want a name for
-# Dept name
-# Generator random names and characters
-
-unique_EC2s = []
-
-int(input("How many EC2 instances do you want names for?" ))
-print(unique EC2 names)
-
-input("What is the name of your department?" )
-
-
-randon.randint()
-random.randrange()
-random.choice()   ??
-random.choices()   ??
-
-
-def gen_range(stop, start=1, step=1):
-    num = start
-    while num <= stop:
-        yield num
-        num += step
+def ec2_names():
+   
+    number = (int(input("How many EC2 instances do you want names for?" )))
+    department = (input("What is the name of your department?" ))
     
-    for num in gen_range( )
+    instances = []
+    
+    for n in range(number):
+    
+        res = ''.join(random.choices(string.ascii_lowercase + string.digits, k=7))
+        name = department + "-" + res
+        instances.append(name)
+     
+    print(instances)
+            
+     
+print(ec2_names())
+
